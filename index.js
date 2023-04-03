5cdddec5be20c84c3e98dbda1cfc3f95af448280
Wayne <1305738589@qq.com>
2023-04-03 14:53:11 +0800

    优化项目结构

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
-import test from './assets/images/test.JPG'
 
-const App = () => {
-  return <div>
-    react
-    <img src={test} alt="" style={{width:300}}/>
-  </div>
-};
+import App from "./src/App";
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
 root.render(<App></App>)
