8f40747d8bd2dad3c46e4542c681381e91eb0f54
Wayne <1305738589@qq.com>
2023-04-07 09:43:09 +0800

    添加了footer

--- src/Home/sections/footerSection/FooterSection.js
+++ src/Home/sections/footerSection/FooterSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 const FooterSection = () => {
   return (
     <div style={{ width: '100%', height: '60px', color: '#000',textAlign:'center',lineHeight:'60px'}}>
-      <p>XXXXXXXX</p>
+      <a href="https://beian.miit.gov.cn">蜀ICP备2022013939号-1</a>
     </div>
   )
 }
