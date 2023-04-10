456e47950a74c28fe5b5d53ec1618f1b27309cfd
Wayne <1305738589@qq.com>
2023-04-10 20:44:55 +0800

    添加了`写文章`的路由

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Home from './Home/Home.js'
 import DocsPage from "./Pages/DocsPage/DocsPage.js";
 import DocDetail from "./Pages/DocsPage/DocDetail.js";
+import Draft from './Pages/Draft/Draft.js'
 
 import {
   Route, Routes
@@ -13,9 +14,11 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<DocsPage />}></Route>
+        <Route path='/' element={<Draft />}></Route>
         <Route path='/docs' element={<DocsPage />}></Route>
         <Route path='/docs/:docId' element={<DocDetail />}></Route>
+        <Route path='/draft/:docId' element={<Draft />}></Route>
+        <Route path='/draft/new' element={<Draft />}></Route>
       </Routes>
     </>
   );
