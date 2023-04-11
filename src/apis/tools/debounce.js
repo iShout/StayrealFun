8ca6a9413073e410126b7c69d4fb0d1a15d0e168
Wayne <1305738589@qq.com>
2023-04-11 14:46:40 +0800

    优化了防抖方法

--- src/apis/tools/debounce.js
+++ src/apis/tools/debounce.js
@@ -1,13 +1,13 @@
-const debounce = function (func, wait = 300) {
+const debounce = function (func, wait = 1000) {
   let timer = null;
   return function (args) {
-    const self = this;
+    const self = this || window;
     if (timer) {
       clearTimeout(timer)
       timer = null
     }
     timer = setTimeout(() => {
-      func.apply(self,args)
+      func.apply(self,args&&[...args])
       timer = null
     }, wait);
   };
